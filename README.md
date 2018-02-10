c7f8c916baac771bb335222dd94d2992383a5158
Michiel Dral <m.c.dral@gmail.com>
2018-02-10 15:16:21 +0100

    Readmeeeee

---
+++ README.md
@@ -0,0 +1,17 @@
+### Setup
+
+```
+$ npm install
+```
+
+```
+$ npm start
+```
+
+```
+* Use code in client.js on your page *
+```
+
+**really?** yes  
+**No setup instructions for Chrome or phanto-** no  
+**Cool** yes
--- client.js
+++ client.js
@@ -1,4 +1,6 @@
-let REPORT_TO_PDF_URL = 'http://localhost:5000';
+//let REPORT_TO_PDF_URL = 'http://localhost:5000'; // For testing locally
+let REPORT_TO_PDF_URL = 'https://report-to-pdf.herokuapp.com';
+
 let download_pdf_from_html = async ({ html_body, pdf_options, filename }) => {
   let resp = await fetch(`${REPORT_TO_PDF_URL}/request_pdf_path`, {
     method: 'POST',
@@ -10,3 +12,11 @@ let download_pdf_from_html = async ({ html_body, pdf_options, filename }) => {
   let { pdf_path } = await resp.json();
   window.location.href = `${REPORT_TO_PDF_URL}${pdf_path}`;
 }
+
+/*
+download_pdf_from_html({
+  html_body: '<h1>Hey!</h1>',
+  pdf_options: { landscape: true },
+  filename: 'my-report.pdf',
+})
+*/
