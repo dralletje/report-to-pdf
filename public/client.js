7428965e4d1e7accb0011d031d694e84698ed6f0
Michiel Dral <m.c.dral@gmail.com>
2018-02-16 17:56:38 +0100

    Hackily make it into the editor too

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-//let REPORT_TO_PDF_URL = 'http://localhost:5000'; // For testing locally
+// let REPORT_TO_PDF_URL = 'http://localhost:5000'; // For testing locally
 let REPORT_TO_PDF_URL = 'https://report-to-pdf.herokuapp.com';
 
 let download_pdf_from_html = async ({ html_body, pdf_options, filename }) => {
@@ -17,6 +17,29 @@ window.download_pdf_from_html = download_pdf_from_html;
 
 /*
 download_pdf_from_html({
+  html_body: '<h1>Hey!</h1>',
+  pdf_options: { landscape: true },
+  filename: 'my-report.pdf',
+})
+*/
+
+
+let download_pdf_from_url = async ({ url, pdf_options, filename }) => {
+  let resp = await fetch(`${REPORT_TO_PDF_URL}/request_pdf_path_by_url`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ url, pdf_options, filename }),
+  });
+  let { pdf_path } = await resp.json();
+  window.location.href = `${REPORT_TO_PDF_URL}${pdf_path}`;
+}
+
+window.download_pdf_from_url = download_pdf_from_url;
+
+/*
+download_pdf_from_url({
   html_body: '<h1>Hey!</h1>',
   pdf_options: { landscape: true },
   filename: 'my-report.pdf',
